c8a210f1ad52bbeed315ee1e5d587cff3fa970c9
lvlifeng <lvlifeng@tuzhanai.com>
2022-05-12 21:02:17 +0800

    :-D

---
+++ src/main/kotlin/com/lvlifeng/jenkinshelper/helper/NotifierHelper.kt
@@ -0,0 +1,33 @@
+package com.lvlifeng.jenkinshelper.helper
+
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+import com.sun.istack.Nullable
+
+/**
+ *
+ *
+ * @author Lv Lifeng
+ * @date 2022-05-12 18:49
+ */
+object NotifierHelper {
+
+    fun notifyError(@Nullable project: Project?, content: String?) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+            .createNotification(content!!, NotificationType.ERROR)
+            .notify(project)
+    }
+
+    fun notifyInfo(@Nullable project: Project?, content: String?) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+            .createNotification(content!!, NotificationType.INFORMATION)
+            .notify(project)
+    }
+
+    fun notifyWarn(@Nullable project: Project?, content: String?) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+            .createNotification(content!!, NotificationType.WARNING)
+            .notify(project)
+    }
+}
--- src/main/kotlin/com/lvlifeng/jenkinshelper/jenkins/AccountState.kt
+++ src/main/kotlin/com/lvlifeng/jenkinshelper/jenkins/AccountState.kt
@@ -1,5 +1,9 @@
 package com.lvlifeng.jenkinshelper.jenkins
 
+import com.intellij.credentialStore.CredentialAttributes
+import com.intellij.credentialStore.Credentials
+import com.intellij.credentialStore.generateServiceName
+import com.intellij.ide.passwordSafe.PasswordSafe
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
@@ -9,6 +13,8 @@ import com.lvlifeng.jenkinshelper.Bundle
 import com.lvlifeng.jenkinshelper.jenkins.Jenkins.Companion.server
 import com.offbytwo.jenkins.JenkinsServer
 import org.apache.commons.lang3.StringUtils
+import org.jetbrains.annotations.Nullable
+
 
 /**
  *
@@ -17,7 +23,7 @@ import org.apache.commons.lang3.StringUtils
  * @date 2022-05-08 03:30
  */
 @State(name = "AccountState", storages = [Storage("\$APP_CONFIG$/jenkins-helper-settings-persistentstate.xml")])
-class AccountState : PersistentStateComponent<AccountState?> {
+class AccountState : PersistentStateComponent<AccountState> {
 
     private var nickName: String? = null
         get() = field
@@ -52,7 +58,7 @@ class AccountState : PersistentStateComponent<AccountState?> {
             get() = ServiceManager.getService(AccountState::class.java)
 
         val defaultAc: Jenkins
-            get() = Jenkins(Bundle.message("defaultAccount"), null, null,null)
+            get() = Jenkins(Bundle.message("defaultAccount"), "", "","")
 
         fun addAccount(newJenkins: Jenkins, currentJenkins: Jenkins?): Boolean {
             val newJenkinsServer: JenkinsServer = server(newJenkins)
@@ -89,4 +95,26 @@ class AccountState : PersistentStateComponent<AccountState?> {
     fun removeAccount(jk: Jenkins) {
         jks.remove(jk)
     }
+
+    fun getPassword(urlAndUserName: String): @Nullable String? {
+//        val key: String? = null // e.g. serverURL, accountID
+        val credentialAttributes = createCredentialAttributes(urlAndUserName)
+
+//        val credentials: @Nullable Credentials? = credentialAttributes?.let { PasswordSafe.instance.get(it) }
+//        if (credentials != null) {
+//            val password: @NotNull String? = credentials.getPasswordAsString()
+//        }
+        return credentialAttributes?.let { PasswordSafe.instance.getPassword(it) }
+    }
+
+    private fun createCredentialAttributes(key: String): CredentialAttributes? {
+        return CredentialAttributes(generateServiceName(Bundle.message("subSytem"), key)
+        )
+    }
+    fun savePassword() {
+        val credentialAttributes = createCredentialAttributes(serverId) // see previous sample
+        val credentials = Credentials(username, password)
+        getInstance.getInstance().set(credentialAttributes, credentials)
+    }
+
 }
