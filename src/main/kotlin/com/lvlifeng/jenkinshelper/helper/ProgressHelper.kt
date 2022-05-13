4cde3595b710abc28f463b8c727035eeabd0d02e
Lv Lifeng <lvlifen9@gmail.com>
2022-05-13 12:30:23 +0800

    :-D

---
+++ src/main/kotlin/com/lvlifeng/jenkinshelper/helper/ProgressHelper.kt
@@ -0,0 +1,25 @@
+package com.lvlifeng.jenkinshelper.helper
+
+import cn.hutool.core.collection.CollectionUtil
+import com.google.common.collect.Lists
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Function
+import java.util.function.Predicate
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+/**
+ *
+ *
+ * @author Lv Lifeng
+ * @date 2022-05-13 12:17
+ */
+class ProgressHelper {
+
+
+    companion object {
+        
+    }
+}
